d8015229855080dceac218574c01363997c72537
Josh Richard Dyke <jrdyke@silk2.uvm.edu>
2015-09-25 11:25:46 -0400

    greetings set

--- test.php
+++ test.php
@@ -4,11 +4,17 @@ function getGreeting($hrOfDay){
     
     $hrOfDay = (int) $hrOfDay;
     switch ($hrOfDay){
-        case $hrOfDay >= 8 && $hrOfDay <= 11:
+        case $hrOfDay >= 6 && $hrOfDay <= 11:
             $greeting = "Good morning!";
             break;
-        case $hrOfDay > 11:
-            $greeting = "Good day";
+        case $hrOfDay >= 12 && $hrOfDay <= 17:
+            $greeting = "Good afternoon!";
+            break;
+        case $hrOfDay >= 18 && $hrOfDay <= 20:
+            $greeting = "Good evening!";
+            break;
+        case $hrOfDay >= 21 || $hrOfDay <= 5:
+            $greeting = "Good night!";
             break;
         
     }
@@ -18,8 +24,11 @@ function getGreeting($hrOfDay){
 
 print '<p>START';
 
-print '<p>Passing 8AM: Greeting = ' . getGreeting(17);
+print '<p>Passing 8AM: Greeting = ' . getGreeting(8);
+print '<p>Passing 12PM: Greeting = ' . getGreeting(12);
+print '<p>Passing 5PM: Greeting = ' . getGreeting(18);
+print '<p>Passing 10PM: Greeting = ' . getGreeting(22);
 
-print '<p>End';
+print '<p>END';
 
 ?>
